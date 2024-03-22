d1229a1248580765ca13713b78b0acb4b05a71fb
ikiwi <ikiwicc@gmail.com>
2024-03-23 00:29:44 +0800

    the first commit

---
+++ src/modules/i18n/components/locale-selector.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { availableLocales, locale } = useI18n();
+
+const localesLong: Record<string, string> = {
+  en: 'English',
+  zh: '中文',
+};
+
+const localeOptions = computed(() =>
+  availableLocales.map(locale => ({
+    label: localesLong[locale] ?? locale,
+    value: locale,
+  })),
+);
+</script>
+
+<template>
+  <c-select
+    v-model:value="locale"
+    :options="localeOptions"
+    placeholder="Select a language"
+    w-100px
+  />
+</template>
